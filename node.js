f54bae22a4dbfc20784ec86183c405ec977b2c7f
NATAN BRENO FERREIRA <natan_b_ferreira@estudante.sesisenai.org.br>
2025-05-22 09:45:24 -0300

    Rota para Adicionar Log

--- node.js
+++ node.js
@@ -11,3 +11,21 @@ function registrarLog(Nome){
 }
 
 module.export = registrarLog.Log;
+
+const express = require ('express');
+const requireLog = require('./logs');
+const app = express();
+app.use(express.json());
+
+app.post('./log' , (req,res) =>{
+    const {nome} = req.body;
+    if (!nome){
+        return res.status(400).json( {error: "Precisa do nome do aluno"});
+    }
+})
+
+const PORT = 8900
+
+app.listen(PORT,  () => {
+    console.log("Servidor esta rodando")
+})
